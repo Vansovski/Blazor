6205df5f9875e2631c065b0477142651c04ff0f3
Marcelo Vansovski de Melo <marcelo_vansovski@hotmail.com>
2024-03-23 18:12:55 -0300

    Correção no modo de renderização

--- BlazorShop/BlazorShop/BlazorShop/Program.cs
+++ BlazorShop/BlazorShop/BlazorShop/Program.cs
@@ -1,20 +1,15 @@
-using BlazorShop.web.Client.Pages;
-using BlazorShop.web.Components;
+using BlazorShop.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -27,7 +22,6 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(BlazorShop.web.Client._Imports).Assembly);
+    .AddInteractiveServerRenderMode();
 
 app.Run();
